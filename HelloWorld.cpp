5701a9ac36032171cd3b2cd4539737ab22663429
Juan Manuel <juan.valdez4064@alumnos.udg.mx>
2025-01-29 00:57:06 -0600

    Primer commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+/*******PRESENTACION********
+NOMBRE: Juan Manuel Valdez Lopez
+FECHA: 29 de Enero de 2025
+PROGRAMA: HolaMundo.cpp
+CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+PROFESOR: Sergio Frnaco Casillas
+DESCRIPCION: En este programa imprimimos en la terminal "Hola, Mundo"
+********************************************************************/
+
+
+#include <iostream>
+int main(){
+    std::cout << "Hola, Mundo" << std::endl;
+    return 0;
+}
