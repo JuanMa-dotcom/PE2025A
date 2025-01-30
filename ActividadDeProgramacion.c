567d19c8e3ad7d8196600d18ce33697513e2bab6
Juan Manuel <juan.valdez4064@alumnos.udg.mx>
2025-01-30 15:12:14 -0600

    Segundo commit

---
+++ ActividadDeProgramacion.c
@@ -0,0 +1,19 @@
+/*******PRESENTACION********
+NOMBRE: Juan Manuel Valdez Lopez
+FECHA:30 de Enero de 2025
+PROGRAMA: Actividad de programacion.c
+CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+PROFESOR: Sergio Franco Casillas
+DESCRIPCION: En este programa imprimimos en la terminal datos personales"
+********************************************************************/
+#include <stdio.h>
+int main(){
+
+    printf("Nombre: Juan Manuel Valdez Lopez\n");
+    printf("Edad: 20 anios\n");
+    printf("Lugar de residencia: Arandas, Jalisco\n");
+    printf("Fecha: 30/01/2025\n");
+    printf("Carrera: Ing. en Computacion\n");
+    printf("Semestre: Segundo\n");
+        return 0;
+}
