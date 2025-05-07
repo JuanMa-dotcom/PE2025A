f18a21dc326494280c7c4f490445355169dbb805
Juan Manuel <juan.valdez4064@alumnos.udg.mx>
2025-05-07 02:10:52 -0600

    Funciones del proyecto

---
+++ PF/Funciones.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <limits.h>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+#include <cmath>
+#include <iomanip>
+#include <cstring> // Para strchr
+using namespace std;
+
+// Prototipos de funciones
+void ejercicio1_datos_personales();
+void ejercicio2_caracteres_escape();
+void ejercicio3_operaciones_numero();
+void ejercicio4_fibonacci();
+void ejercicio5_multiplos();
+void ejercicio6_suma_cuadrados();
+void ejercicio7_suma_primos();
+void ejercicio8_manejo_arreglos();
+void ejercicio9_manejo_matrices();
+void ejercicio10_operaciones_conjuntos();
+void ejercicio11_secuencia_collatz();
+void ejercicio12_juego_ahorcado();
+void ejercicio13_torres_hanoi();
+void ejercicio14_palindromo();
+
